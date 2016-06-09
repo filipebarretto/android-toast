d27013ea637b2bced815bf637565b32f87886c3f
Filipe Barretto <filipebarretto@gmail.com>
2016-06-09 09:54:42 -0300

    1.1.0

--- AndroidToast/app/src/androidTest/java/com/filipebarretto/androidtoast/InstrumentationTest.java
+++ AndroidToast/app/src/androidTest/java/com/filipebarretto/androidtoast/InstrumentationTest.java
@@ -1,35 +1,22 @@
 package com.filipebarretto.androidtoast;
 
-import org.hamcrest.Matcher;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.internal.builders.AllDefaultPossibilitiesBuilder;
 import org.junit.runner.RunWith;
 
-import android.support.test.espresso.Root;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.support.test.runner.AndroidJUnitRunner;
-import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.SmallTest;
-import android.util.Log;
-import android.widget.Toast;
 
 import com.filipebarretto.androidtoast.custom.ToastMatcher;
-
-import junit.framework.TestSuite;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.core.deps.guava.base.CharMatcher.is;
 import static org.hamcrest.Matchers.not;
-import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -47,9 +34,9 @@ public class InstrumentationTest {
     @Test
     public void yodaButtonClick() throws Exception {
         lock.await(2000, TimeUnit.MILLISECONDS);
-        onView(withId(R.id.yodaButton)).perform(click());
+        onView(withId(R.id.biscoitoButton)).perform(click());
 
-        onView(withText(R.string.yoda_toast)).inRoot(new ToastMatcher())
+        onView(withText(R.string.biscoito_toast)).inRoot(new ToastMatcher())
                 .check(matches(isDisplayed()));
 
     }
@@ -57,9 +44,9 @@ public class InstrumentationTest {
     @Test
     public void vaderButtonClick() throws Exception {
         lock.await(2000, TimeUnit.MILLISECONDS);
-        onView(withId(R.id.vaderButton)).perform(click());
+        onView(withId(R.id.bolachaButton)).perform(click());
 
-        onView(withText(R.string.vader_toast)).inRoot(new ToastMatcher())
+        onView(withText(R.string.bolacha_toast)).inRoot(new ToastMatcher())
                 .check(matches(isDisplayed()));
 
     }
--- AndroidToast/app/src/main/java/com/filipebarretto/androidtoast/MainActivity.java
+++ AndroidToast/app/src/main/java/com/filipebarretto/androidtoast/MainActivity.java
@@ -2,57 +2,37 @@ package com.filipebarretto.androidtoast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.analytics.FirebaseAnalytics;
-
 public class MainActivity extends AppCompatActivity {
 
-    private FirebaseAnalytics mFirebaseAnalytics;
-
-    private Button yodaButton;
-    private Button vaderButton;
+    private Button mBiscoitoButton;
+    private Button mBolachaButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Obtain the FirebaseAnalytics instance.
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        mBiscoitoButton = (Button) findViewById(R.id.biscoitoButton);
+        mBolachaButton = (Button) findViewById(R.id.bolachaButton);
 
-        yodaButton = (Button) findViewById(R.id.yodaButton);
-        vaderButton = (Button) findViewById(R.id.vaderButton);
-
-        Log.i("TEST", String.valueOf(Toast.LENGTH_SHORT));
-
-        yodaButton.setOnClickListener(new View.OnClickListener() {
+        mBiscoitoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                customEvent("y001", "YODA");
-                Toast.makeText(MainActivity.this, getString(R.string.yoda_toast), Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, getString(R.string.biscoito_toast), Toast.LENGTH_SHORT).show();
             }
         });
 
-        vaderButton.setOnClickListener(new View.OnClickListener() {
+        mBolachaButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                customEvent("v001", "VADER");
-                Toast.makeText(MainActivity.this, getString(R.string.vader_toast), Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, getString(R.string.bolacha_toast), Toast.LENGTH_SHORT).show();
             }
         });
 
-    }
-
-    private void customEvent(String id, String name) {
-        Bundle bundle = new Bundle();
-        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, id);
-        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, name);
-        mFirebaseAnalytics.logEvent("click", bundle);
     }
 
 
